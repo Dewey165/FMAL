98b603e76941797c17ce0dc3fb1a7c6a2b4f51ed
einargizz <einarog05@ru.is>
2016-02-09 21:58:54 +0000

    bull

--- src/Parser.java
+++ src/Parser.java
@@ -12,3 +12,37 @@ public class Parser {
 		
 	}
 }
+
+
+enum TokenCode { LPAREN, RPAREN, INT, PLUS, MINUS};
+TokenCode nextToken;
+void parser() {
+	nextToken = lex();
+	P();
+}
+void P() {
+	if (nextToken == LPAREN) {
+		nextToken = lex();
+		Expr();
+		if (nextToken == RPAREN)
+			nextToken = lex();
+			else error();
+		}
+		else {
+			if (nextToken == int)
+			nextToken = lex();
+			else error();
+		}
+	}
+	void Expr() {
+		Oper();
+		P();
+		P();
+	}
+	void Oper() {
+	if (nextToken == PLUS)
+	nextToken = lex();
+	else if (nextToken == MINUS)
+	nextToken = lex();
+	else error();
+}
