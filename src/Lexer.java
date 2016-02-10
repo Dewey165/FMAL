858c6c961a58a8e6fe7059c81335d8096e5b918f
Dewey165 <agi2211@gmail.com>
2016-02-10 13:18:39 +0000

    Update Lexer.java

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 
 public class Lexer {
 	
 	public void nextToken()
 	{
-		// scan stdin looking for patterns that match one of 
-		// the tokens 1). 
-		
+		Scanner scanner = new Scanner (System.in);
+        	String lexeme = scanner.next();
+		//Eitthvað svona, en scanner virkar, spurning með að senda í tokenið..
+		Token tCode = new Token.deCode(s);
+        	System.out.println(tCode.tCode);
 	}
 }
