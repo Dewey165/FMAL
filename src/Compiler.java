a84dbb87c75e243516d75ddd16f743d2676f4eff
einargizz <einarog05@ru.is>
2016-02-10 00:53:19 +0000

    Sauðakóði fyrir Parser.

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,6 +1,9 @@
 
 public class Compiler {
-	Lexer myLexer = new Lexer();
-	Parser myParser = new Parser(myLexer);
-	myParser.parse();
+	
+	public void main() {
+		Lexer myLexer = new Lexer();
+		Parser myParser = new Parser(myLexer);
+		myParser.parse();
+	}	
 }
--- src/Parser.java
+++ src/Parser.java
@@ -5,10 +5,10 @@ public class Parser {
 	
 	public void parse() {
 		// TODO Auto-generated constructor stub
-		/*
+		
 		token = lex.nextToken();
 		Statements();
-		/*
+		
 	}
 	
 	private void Statements() {
@@ -17,11 +17,13 @@ public class Parser {
  		break;
  		}
  		else {
- 			token = lex.nextToken();
  			Statement();
  			if (token == SEMICOL) {
  				token = lex.nextToken();
  				Statements();
+ 			}
+ 			else {
+ 				Error();
  			}
  		}
 		*/
@@ -35,30 +37,67 @@ public class Parser {
  				token = lex.nextToken();
  				Expr();
  			}
+ 			else {
+ 				Error();
+ 			}
  		}
  		else if (token == PRINT) {
  			token = lex.nextToken();
- 			if (token == ASSIGN) {
- 			
+ 			if (token == ID) {
+ 				token = lex.nextToken();
+ 			}
+ 			else {
+ 				Error();
+ 			}
+ 		}
+ 		else {
+ 			Error();
  		}
 		*/
 	}
 	
 	private void Expr() {
 		/*
- 		
+		Term();
+		if (token == ADD || token == SUB) {
+			token = lex.nextToken();
+			Expr();
+		}
 		*/
 	}
 	
 	private void Term() {
 		/*
- 		
+ 		Factor();
+		if (token == MULT) {
+			token = lex.nextToken();
+			Term();
+		}
 		*/
 	}
 	
 	private void Factor() {
 		/*
- 		
+ 		if (token == INT) {
+ 			token = lex.nextToken();
+ 		}
+ 		else if (token == ID) {
+ 			token = lex.nextToken();
+ 		}
+ 		else if (token == LPAREN) {
+ 			token = lex.nextToken();
+ 			Expr();
+ 			token = lex.nextToken();
+ 			if (token == LPAREN) {
+ 				token = lex.nextToken();
+ 			}
+ 			else {
+ 				Error();
+ 			}
+ 		}
+ 		else {
+ 			Error();
+ 		}
 		*/
 	}
 	
