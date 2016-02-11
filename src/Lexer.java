e3e16081ae3e8e0cfff6e85f36f1beb778392c42
Dewey165 <agi2211@gmail.com>
2016-02-11 00:44:28 +0000

    lexer kominn
    koinn

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,32 +1,88 @@
-import java.util.Scanner;
-
+import java.io.IOException;
+import java.io.StreamTokenizer;
+import java.util.*;
 public class Lexer {
 	
-	Scanner scanner = new Scanner (System.in);
+	StreamTokenizer st = new StreamTokenizer(System.in);
 	
-	static boolean semicol = false;
-		
-	//String?Token?
+	/*
+	Scanner scanner = new Scanner (System.in);
+	static boolean readIntoQueue = true;
+	private static Queue<String> queue = new LinkedList<String>();
+	*/
 	public Token nextToken()
 	{
 		String lexeme;
-		if(semicol)
-		{
-			lexeme = ";";
-			semicol = false;
-		}
-		else 
-		{
-			lexeme = scanner.next();
-			char[] temp = lexeme.toCharArray();
-			if( temp[lexeme.length()-1] == ';')
-			{
-				semicol = true;
-				int length = lexeme.length() - 1;
-				lexeme = lexeme.substring(0,length);
+		Token tokenCode = null;
+		int tokenType;
+		try {
+			tokenType = st.nextToken();
+			lexeme = st.toString();
+			
+			
+			
+			//tokenType == StreamTokenizer.TT_EOF;
+			//tokenType = st.nextToken()) 
+		
+			switch(tokenType) {
+				case StreamTokenizer.TT_NUMBER:
+					double temp = st.nval;
+					int temp1 = (int)Math.round(temp);
+					lexeme = "" + temp1; 
+					tokenCode = new Token(lexeme);
+					tokenCode.tCode =  Token.TokenCode.INT;
+					break;
+				case StreamTokenizer.TT_WORD:
+					lexeme = st.sval;
+					tokenCode = new Token(lexeme);
+					//System.out.println("identifier " + st.sval);
+					if(lexeme.matches("print"))
+					{
+						tokenCode.tCode = Token.TokenCode.PRINT;
+					}
+					else if(lexeme.matches("end"))
+					{
+						tokenCode.tCode =  Token.TokenCode.END;
+					}
+					else
+					{
+						tokenCode.tCode =  Token.TokenCode.ID;
+					}
+					break;
+				default:
+					lexeme = "" +(char)tokenType;
+					tokenCode = new Token(lexeme);
+					if((char)tokenType == '=')					{tokenCode.tCode =  Token.TokenCode.ASSIGN;}
+					else if((char)tokenType == ';')				{tokenCode.tCode =  Token.TokenCode.SEMICOL;}
+					else if((char)tokenType == '+')				{tokenCode.tCode =  Token.TokenCode.ADD;}
+					else if((char)tokenType == '-')				{tokenCode.tCode =  Token.TokenCode.SUB;}
+					else if((char)tokenType == '*')				{tokenCode.tCode =  Token.TokenCode.MULT;}
+					else if((char)tokenType == '(')				{tokenCode.tCode =  Token.TokenCode.LPAREN;}
+					else if((char)tokenType == ')')				{tokenCode.tCode =  Token.TokenCode.RPAREN;}
+					else 										{tokenCode.tCode =  Token.TokenCode.ERROR;}
 			}
+		} 
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return tokenCode;
+	}
+		/*if(readIntoQueue)
+		{
+			readIntoQueue();
+		}
+		for(int i = 0; i < queue.size(); i++)
+		{
+			System.out.println(queue.poll());
 		}
 		
+
+		lexeme = queue.poll();
+		//char[] temp = lexeme.toCharArray();
+		
+		
+		lexeme = st.nextToken().toString();
 		char lexChar = lexeme.charAt(0);
 		Token tokenCode = new Token(lexeme);
 		
@@ -41,8 +97,9 @@ public class Lexer {
 		else if(lexChar == '*')				{tokenCode.tCode =  Token.TokenCode.MULT;}
 		else if(lexChar == '(')				{tokenCode.tCode =  Token.TokenCode.LPAREN;}
 		else if(lexChar == ')')				{tokenCode.tCode =  Token.TokenCode.RPAREN;}
-		else 								{tokenCode.tCode =  tokenCode.tCode.ERROR;}
+		else 								{tokenCode.tCode =  Token.TokenCode.ERROR;}
 		
 		return tokenCode;
 	}
+	*/
 }
